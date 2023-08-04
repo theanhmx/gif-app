1d3daebceca32192f78a86440087aef174ee9a76
Diệp Thế Anh <theanh@Dieps-MacBook-Pro.local>
2023-08-04 18:14:48 +0700

    change homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { GifDetail, Root, Upload } from './routes';
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { GifDetail, Root, Upload } from "./routes";
 
-import { SearchContextManager } from '@giphy/react-components';
-import { gf } from './api/giphyFetch';
+import { SearchContextManager } from "@giphy/react-components";
+import { gf } from "./api/giphyFetch";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Root />,
-	},
-	{
-		path: '/detail/:id',
-		element: <GifDetail />,
-		loader: async ({ params }) => {
-			const { data } = await gf.gif(params.id || '');
-			return data;
-		},
-	},
-	{
-		path: '/upload',
-		element: <Upload />,
-	},
+  {
+    path: "/gif-app",
+    element: <Root />,
+  },
+  {
+    path: "/detail/:id",
+    element: <GifDetail />,
+    loader: async ({ params }) => {
+      const { data } = await gf.gif(params.id || "");
+      return data;
+    },
+  },
+  {
+    path: "/upload",
+    element: <Upload />,
+  },
 ]);
 
 function App() {
-	return (
-		<div className="App">
-			<SearchContextManager apiKey="TA0egc9FO0OiKVDPqhOheLQM5ro36A5D">
-				<RouterProvider router={router} />
-			</SearchContextManager>
-		</div>
-	);
+  return (
+    <div className="App">
+      <SearchContextManager apiKey="TA0egc9FO0OiKVDPqhOheLQM5ro36A5D">
+        <RouterProvider router={router} />
+      </SearchContextManager>
+    </div>
+  );
 }
 
 export default App;
