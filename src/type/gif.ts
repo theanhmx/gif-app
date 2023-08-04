e9b53fb34391fff3c2d8779c32d14575de514cd7
Diệp Thế Anh <theanh@Dieps-MacBook-Pro.local>
2023-08-04 18:07:44 +0700

    Deploy React app to GitHub Pages again

--- src/type/gif.ts
+++
@@ -1,6 +0,0 @@
-export type DraftGif = {
-	file: File;
-	tags: string; //A comma delimited list of tags to be applied to this GIF
-	source_post_url: string; //The URL of the webpage on which this GIF was found
-	id?: string; //The unique ID of the GIF
-};
